e1c8eec21f911dab7df0be36e40af9464eb8c15f
SOHRAB5129 <sohrabhusain123@gmail.com>
2020-02-24 16:30:02 +0700

    authorization server using oauth2, without Jwt

---
+++ auth-server/src/main/java/com/sohrab/rentcloud/auth/authserver/conf/UserConfiguration.java
@@ -0,0 +1,20 @@
+package com.sohrab.rentcloud.auth.authserver.conf;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class UserConfiguration extends GlobalAuthenticationConfigurerAdapter {
+
+	PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	
+	@Override
+	public void init(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+		.withUser("sohrab").password(passwordEncoder.encode("shah123")).roles("USER", "ADMIN", "MANAGER").authorities("CAN_READ", "CAN_WRITE", "CAN_DELETE").and()
+		.withUser("nazreen").password(passwordEncoder.encode("naz123")).roles("USER").authorities("CAN_READ", "CAN_WRITE");
+	}
+}
--- authorization-server/src/main/java/com/sohrab/rentcloud/authorizationserver/config/AuthorizationServerConfiguration.java
+++ authorization-server/src/main/java/com/sohrab/rentcloud/authorizationserver/config/AuthorizationServerConfiguration.java
@@ -1,44 +1,49 @@
-package com.sohrab.rentcloud.auth.authserver.conf;
+package com.sohrab.rentcloud.authorizationserver.config;
+
+import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
 @Configuration
-public class AuthServerConfiguration extends WebSecurityConfigurerAdapter implements AuthorizationServerConfigurer {
+public class AuthorizationServerConfiguration implements AuthorizationServerConfigurer {
 
-	@Bean
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		return super.authenticationManagerBean();
-	}
+	@Autowired
+	PasswordEncoder passwordEncoder;
+
+	@Autowired
+	DataSource dataSource;
 
 	@Autowired
 	AuthenticationManager authenticationManager;
 
-	PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	@Bean
+	TokenStore jdbcTokenStore() {
+		return new JdbcTokenStore(dataSource);
+	}
 
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-		security.checkTokenAccess("permitAll()");
+		security.checkTokenAccess("isAuthenticated()").tokenKeyAccess("permitAll()");
 	}
 
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		clients.inMemory().withClient("web").secret(passwordEncoder.encode("webpass")).scopes("READ", "WRITE")
-				.authorizedGrantTypes("password", "authorization_code");
-
+		clients.jdbc(dataSource).passwordEncoder(passwordEncoder);
 	}
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints.tokenStore(jdbcTokenStore());
 		endpoints.authenticationManager(authenticationManager);
 	}
 
